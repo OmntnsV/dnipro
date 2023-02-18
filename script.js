c3dbe01caff391c807c612fae401a1c620719400
OmntnsV <yu.viktor31@gmail.com>
2023-02-18 14:45:59 +0200

    Linking to Git

---
+++ script.js
@@ -0,0 +1,287 @@
+"use strict";
+
+const selectorSecond = document.querySelector('#pathTo');
+
+
+// for (const key in selectorSecond) {
+//     if (Object.hasOwnProperty.call(selectorSecond, key)) {
+//         console.log(key + " " + selectorSecond[key])
+//         delete selectorSecond.key;
+        
+//     }
+// }
+
+
+
+document.querySelector('#path').addEventListener('change', function () {
+
+    if (document.querySelector('#pathTo').hasAttribute('disabled')) {
+        document.querySelector('#pathTo').removeAttribute('disabled');
+    }
+// const toRemove =
+    document.querySelectorAll('.added').forEach((elem) => {
+        elem.remove();
+    });
+
+    // for (let index = 0; index < toRemove.length; index++) {     // Удалитель предыдущих вариантов
+    //     const element = toRemove[index];
+    //     element.remove();
+    //     console.log("Element type is: " + typeof element);
+    // }
+
+    let choise = document.querySelector('#path').value;
+
+    switch (choise) {                                           // Проверяет выбор в первом списке и отправляет функции список дальнейших путей
+
+        case 'pk1':
+            console.log('Выбран пк1');
+            setSelectorOptions(pk1List);
+        break;
+
+        case 'pk2':
+            console.log('Выбран пк2');
+            setSelectorOptions(pk2List);
+        break;
+
+        case 'pk3':
+            console.log('Выбран  пк3');
+            setSelectorOptions(pk3List);
+        break;
+
+        case 'pk4':
+            console.log('Выбран pk4');
+            setSelectorOptions(pk4List);
+        break;
+
+        case 'tpk1':
+            console.log('Выбран тпк1');
+            setSelectorOptions(tpk1List);
+        break;
+
+        case 'tpk2':
+            console.log('Выбран тпк2');
+            setSelectorOptions(tpk2List);
+        break;
+    }
+
+});
+
+
+
+const pk1List = [
+    {value: '3', title: '3-ий станционный путь ст. Покровская'},
+    {value: '4', title: '4-ый станционный путь ст. Покровская'},
+    {value: '1', title: 'Тупик по первому пути ст. Покровская'}
+];
+
+const pk2List = [
+    {value: '4', title: '4-ый станционный путь ст. Покровская'},
+    {value: '3', title: '3-ий станционный путь ст. Покровская'},
+    {value: '2', title: 'Тупик по второму пути ст. Покровская'}
+];
+
+const pk3List = [
+    {value: '1', title: '1-ый станционный путь ст. Покровская'},
+    {value: '2', title: '2-ой станционный путь ст. Покровская'},
+    {value: 'depot1', title: 'Выезд в депо по первому пути'},
+];
+
+const pk4List = [
+    {value: '1', title: '1-ый станционный путь ст. Покровская'},
+    {value: '2', title: '2-ой станционный путь ст. Покровская'},
+    {value: 'depot1', title: 'Выезд в депо по второму пути'},
+];
+
+const tpk1List = [
+    {value: '1', title: '1-ый станционный путь ст. Покровская'}
+];
+
+const tpk2List = [
+    {value: '2', title: '2-ой станционный путь ст. Покровская'}
+];
+
+function setSelectorOptions(displayList) {
+
+    for (let index = 0; index < displayList.length; index++) {
+
+        const element = displayList[index];
+
+        for (const key in element) {
+
+            if (Object.hasOwnProperty.call(element, key)) {
+                const selectorOption = document.createElement('option');
+                const objectElement = element[key];
+                selectorOption.key = objectElement;
+
+                if (key == "title") {
+                    selectorOption.innerText = objectElement;
+                    selectorOption.setAttribute('value', element.value);
+                    console.log("added: " + objectElement);
+                    selectorOption.classList.add('added');
+                    document.getElementById('pathTo').appendChild(selectorOption);
+                    
+                }
+            }
+        }       
+    }
+}
+
+document.querySelector('#pathTo').addEventListener('change', function () {
+    document.getElementById('dots').remove();
+    const from = document.getElementById('path').value;
+    if (from == 'tpk1') {
+        from == 'pk1';
+    } else if (from == 'tpk2') {
+        from == 'pk2';
+    }
+    const to = document.getElementById('pathTo').value;
+    const clipboard = document.createElement('div');
+    clipboard.classList.add("routes__clipboard");
+    const way = '/sopen ' + from + '-' + to;
+    const wayOpacity = document.createElement('op');
+    wayOpacity.innerText = way;
+    wayOpacity.setAttribute('id', 'copy')
+    wayOpacity.classList.add('command__text');
+    document.getElementById('command').appendChild(wayOpacity);
+    document.getElementById('command').appendChild(clipboard);
+    let borderColor = document.getElementById('command');
+    borderColor.style.border = "#fff 1px solid";
+});
+
+document.getElementById('command').addEventListener('click', function(){
+    const textToCopy = document.getElementById('copy').innerText;
+    console.log(textToCopy);
+    console.log(typeof textToCopy);
+    navigator.clipboard.writeText(textToCopy);
+    alert("Команда скопирована");
+});
+
+
+/* COMMUNITY */
+
+
+const employees = document.querySelectorAll('.comm__proff');
+console.log(employees);
+
+
+for (let index = 0; index < employees.length; index++) {
+    const empElement = employees[index];
+    empElement.addEventListener('mouseout', function(empElement){
+        nameDeleter(empElement.srcElement);
+    });
+}
+
+function nameSetter(id, target) {
+    switch (id) {
+        case 'ceo':
+            target.innerText = 'Serge Smile';
+            break;
+        
+        case 'viceCeo':
+            target.innerText = 'Κουνέλι σε ποτήρια, 123';
+            break;
+
+        case 'rev':
+            target.innerText = '...:::Roman:::';
+            break;
+
+        case 'nsd':
+            target.innerText = 'Omntns';
+            break;
+
+        case 'nsps':
+            target.innerText = 'Временно отсутствует';
+            break;
+
+        case 'smi':
+            target.innerText = 'mashinist_ua, Scoofik20k2';
+            break;
+
+        case 'dsp':
+            target.innerText = 'Временно отсутствует';
+            break;
+
+        case 'mi':
+            target.innerText = 'Временно отсутствует';
+            break;
+
+        case 'iomi':
+            target.innerText = 'Nemashinist_metro';
+            break;
+
+        case 'pm':
+            target.innerText = 'Почётный машинист';
+            break;
+            
+        default:
+            
+            break;
+    }
+};
+
+function nameDeleter(element) {
+    const idOfElement = element.id;
+    switch (idOfElement) {
+        case 'ceo':
+            element.innerText = 'Начальник метрополитена';
+            break;
+        
+        case 'viceCeo':
+            element.innerText = 'Зам. начальника метрополитена';
+            break;
+
+        case 'rev':
+            element.innerText = 'Ревизор';
+            break;
+
+        case 'nsd':
+            element.innerText = 'Начальник службы движения';
+            break;
+
+        case 'nsps':
+            element.innerText = 'Начальник службы подв. состава';
+            break;
+
+        case 'smi':
+            element.innerText = 'Старший машинист-инструктор';
+            break;
+
+        case 'dsp':
+            element.innerText = 'Диспетчер';
+            break;
+
+        case 'mi':
+            element.innerText = 'Машинист-инструктор';
+            break;
+
+        case 'iomi':
+            element.innerText = 'И. О. машиниста-инструктора';
+            break;
+
+        case 'pm':
+            element.innerText = 'Почётный машинист';
+            break;
+
+        default:
+            element.innerText = '404 Role not found';
+            break;
+    }
+};
+
+const superAdminsContianer = document.getElementById('superAdmin');
+
+superAdminsContianer.addEventListener('mouseover', function(event){
+    let selectedEmployee;
+    if (event.target.id != null) {
+        selectedEmployee = event.target.id;
+        nameSetter(selectedEmployee, event.target);
+    }
+})
+
+const adminsContianer = document.getElementById('admin');
+
+adminsContianer.addEventListener('mouseover', function(event){
+    if (event.target.id != null) {
+        nameSetter(event.target.id, event.target);
+    }
+})
