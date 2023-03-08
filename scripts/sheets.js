f5c034a7a3feba7d3f2de45ec9980898e96de207
OmntnsV <yu.viktor31@gmail.com>
2023-03-08 13:30:51 +0200

    Added gsheets api and reequire.js

---
+++ scripts/sheets.js
@@ -0,0 +1,30 @@
+/* ADMIN LIST */
+
+
+/* https://docs.google.com/spreadsheets/d/1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU/edit?usp=sharing */
+/* 1hwTzLlfQ4bNDgB_ze5xFqAviQbn7IMh9CutSXO_PGk8 */
+
+const GSheetReader = require('g-sheets-api');
+
+const options = {
+    apiKey: 'AIzaSyA8abGk-iT5GSN7REqahleJ1Ir2tlnVdGo',
+    sheetId: '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU',
+    sheetNumber: 1,
+    sheetName: 'Старшая администрация', // if sheetName is supplied, this will take precedence over sheetNumber
+    returnAllResults: false,
+  }
+
+  console.log('downloading internet');
+
+document.getElementById('ceo').addEventListener('click', function(){
+    console.log('dwnldng trnt');
+    GSheetReader(
+        options,
+        results => {
+          console.log(results);
+        },
+        error => {
+          // OPTIONAL: handle errors here
+        }
+      );
+} )
