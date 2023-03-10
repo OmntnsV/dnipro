e052622d3b91b02a08724b9a939d5d3565a4b520
OmntnsV <yu.viktor31@gmail.com>
2023-03-10 14:26:55 +0200

    fetching google sheet

--- scripts/sheets.js
+++ scripts/sheets.js
@@ -4,25 +4,41 @@
 /* https://docs.google.com/spreadsheets/d/1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU/edit?usp=sharing */
 /* 1hwTzLlfQ4bNDgB_ze5xFqAviQbn7IMh9CutSXO_PGk8 */
 
-const GSheetReader = require('g-sheets-api');
+// const GSheetReader = require('g-sheets-api');
 
-const options = {
-    apiKey: 'AIzaSyA8abGk-iT5GSN7REqahleJ1Ir2tlnVdGo',
-    sheetId: '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU',
-    sheetNumber: 1,
-    sheetName: 'Старшая администрация', // if sheetName is supplied, this will take precedence over sheetNumber
-    returnAllResults: false,
-  }
+// const options = {
+//     apiKey: 'AIzaSyA8abGk-iT5GSN7REqahleJ1Ir2tlnVdGo',
+//     sheetId: '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU',
+//     sheetNumber: 1,
+//     sheetName: 'Старшая администрация', // if sheetName is supplied, this will take precedence over sheetNumber
+//     returnAllResults: false,
+//   }
 
-  console.log('downloading internet');
+//   console.log('downloading internet');
 
-    console.log('dwnldng trnt');
-    GSheetReader(
-        options,
-        results => {
-          console.log(results);
-        },
-        error => {
-          // OPTIONAL: handle errors here
-        }
-      );
+//     console.log('dwnldng trnt');
+//     GSheetReader(
+//         options,
+//         results => {
+//           /* console.log(results); */
+//         },
+//         error => {
+//           // OPTIONAL: handle errors here
+//         }
+//       );
+
+const sheetId = '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU';
+const sheetPage = 'Старшая%20администрация';
+const sheetRange = 'A2:D11';
+
+const sheetLink = 'https://docs.google.com/spreadsheets/d/' + '1hwTzLlfQ4bNDgB_ze5xFqAviQbn7IMh9CutSXO_PGk8' + '/gviz/tq?sheet='
++ '1' + '&range=' + 'A1:C6';
+console.log(sheetLink);
+
+fetch(sheetLink, {crossorigin: true})
+.then (res => res. text ())
+.then (rep => {
+// let data = JSON.parse(rep.substr(47).slice(0, -2));
+
+console.log(rep);
+});
