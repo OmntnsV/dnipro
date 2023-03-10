b5e95e7692af47e86d1b59da939915a740bc5315
OmntnsV <yu.viktor31@gmail.com>
2023-03-10 15:30:40 +0200

    wip

--- scripts/sheets.js
+++ scripts/sheets.js
@@ -1,51 +1,36 @@
 /* ADMIN LIST */
 
-
-/* https://docs.google.com/spreadsheets/d/1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU/edit?usp=sharing */
-/* 1hwTzLlfQ4bNDgB_ze5xFqAviQbn7IMh9CutSXO_PGk8 */
-
-// const GSheetReader = require('g-sheets-api');
-
-// const options = {
-//     apiKey: 'AIzaSyA8abGk-iT5GSN7REqahleJ1Ir2tlnVdGo',
-//     sheetId: '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU',
-//     sheetNumber: 1,
-//     sheetName: 'Старшая администрация', // if sheetName is supplied, this will take precedence over sheetNumber
-//     returnAllResults: false,
-//   }
-
-//   console.log('downloading internet');
-
-//     console.log('dwnldng trnt');
-//     GSheetReader(
-//         options,
-//         results => {
-//           /* console.log(results); */
-//         },
-//         error => {
-//           // OPTIONAL: handle errors here
-//         }
-//       );
-
 const sheetId = '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU';
-const sheetPage = 'Старшая%20администрация';
+const sheetPage = '1';
 const sheetRange = 'A2:D11';
 
 const sheetLink = 'https://docs.google.com/spreadsheets/d/' + sheetId + '/gviz/tq?sheet='
-+ '1' + '&range=' + 'A2:G11';
++ sheetPage + '&range=' + 'A2:G11';
+
+let data;
+let prof;
 
 fetch(sheetLink, {crossorigin: true})
 .then (res => res. text ())
 .then (rep => {
-let data = JSON.parse(rep.substr(47).slice(0, -2));
+data = JSON.parse(rep.substr(47).slice(0, -2));
 console.log(data);
 
 for (let row = 0; row < data.table.rows.length; row++) {
   const element = data.table.rows[row].c;
   if (row != 1) {
-    const nick = element[0].v;
-    const prof = element[3].v;
-    console.log(nick + ': ' + prof);
+    const nick = "" + element[0].v;
+    const job = element[3].v;
+    console.log(job + ': ' + nick);
+    if (prof[job].name != null) {
+        prof[job].name += ", " + nick;
+    }else {
+        prof[job].name = nick;
+    }
   }
 }
 });
+
+function hameGetter(request) {
+
+}
