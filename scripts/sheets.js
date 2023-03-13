f6763e9dc9bec6c96652a7e3f9aa93636061acb7
OmntnsV <yu.viktor31@gmail.com>
2023-03-13 19:23:08 +0200

    wip27

--- scripts/sheets.js
+++ scripts/sheets.js
@@ -32,8 +32,7 @@ for (let row = 0; row < data.table.rows.length; row++) {
 });
 
 function nameGetter(request) {
-  console.log(request);
-  console.log(prof);
+  console.log("Тест " + prof["Разработчик"]);
   if (request === "Начальник Метрополитена") {
     console.log("YPAAA");
     return prof["Разработчик"];
