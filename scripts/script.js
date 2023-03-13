8f27d6b8e0c8a7d9ed58f7261b2c39dc0c6ee1d2
OmntnsV <yu.viktor31@gmail.com>
2023-03-13 18:50:45 +0200

    wip17

--- scripts/script.js
+++ scripts/script.js
@@ -193,7 +193,7 @@ for (let index = 0; index < employees.length; index++) {
 }
 
 function nameSetter(id, target) { // Список администрации
-    if (id == "superAdmin" || id == "admin" || id == null){
+    if (id == "superAdmin" || id == "admin" || id == ""){
         console.log("denied " + id);
     }else {
         console.log("procced" + id);
