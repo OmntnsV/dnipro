20afeaff55b86d582358b31adc09eac4f8247de1
OmntnsV <yu.viktor31@gmail.com>
2023-03-13 20:03:01 +0200

    wip32

--- scripts/sheets.js
+++ scripts/sheets.js
@@ -4,13 +4,16 @@ const sheetId = '1eWicJkKFLfr8pz4CsIL_TDEfbZl9nACHv8i-57LTiiU';
 const sheetPage = '1';
 const sheetRange = 'A2:D11';
 
-const sheetLink = 'https://docs.google.com/spreadsheets/d/' + sheetId + '/gviz/tq?sheet='
+const sheetLink1 = 'https://docs.google.com/spreadsheets/d/' + sheetId + '/gviz/tq?sheet='
 + sheetPage + '&range=' + 'A2:G11';
+
+const sheetLink2 = 'https://docs.google.com/spreadsheets/d/' + sheetId + '/gviz/tq?sheet='
++ 2 + '&range=' + 'A4:G11';
 
 let data;
 let prof = {};
 
-fetch(sheetLink, {crossorigin: true})
+fetch(sheetLink1, {crossorigin: true})
 .then (res => res. text ())
 .then (rep => {
 data = JSON.parse(rep.substr(47).slice(0, -2));
@@ -28,6 +31,27 @@ for (let row = 0; row < data.table.rows.length; row++) {
         prof[job] = nick;
     }
   }
+}
+});
+
+fetch(sheetLink2, {crossorigin: true})
+.then (res => res. text ())
+.then (rep => {
+data = JSON.parse(rep.substr(47).slice(0, -2));
+console.log(data);
+
+for (let row = 0; row < data.table.rows.length; row++) {
+  const element = data.table.rows[row].c;
+
+    const nick = "" + element[0].v;
+    const job = element[3].v;
+    console.log(job + ': ' + nick);
+    if (prof[job] != undefined) {
+        prof[job] += ", " + nick;
+    }else {
+        prof[job] = nick;
+    }
+
 }
 });
 
