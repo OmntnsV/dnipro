c0c3d223b672153541d585ab31d703b20fb74a9a
OmntnsV <yu.viktor31@gmail.com>
2023-03-13 20:09:06 +0200

    wip33

--- scripts/sheets.js
+++ scripts/sheets.js
@@ -37,7 +37,7 @@ for (let row = 0; row < data.table.rows.length; row++) {
 fetch(sheetLink2, {crossorigin: true})
 .then (res2 => res2. text ())
 .then (rep2 => {
-data = JSON.parse(rep.substr(47).slice(0, -2));
+data = JSON.parse(rep2.substr(47).slice(0, -2));
 console.log(data);
 
 for (let row = 0; row < data.table.rows.length; row++) {
